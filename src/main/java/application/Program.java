a777a923ae11cfb5dfe6dd5cb9a84bebd2a84dce
Pedro Henrique Hermes <pedrohhermes2006@gmail.com>
2026-03-23 13:03:56 -0300

    Department e Seller Dao

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -16,7 +16,7 @@ public class Program {
 
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.parse("15/10/2000", fmt), 3000.00, obj);
-        
+        System.out.println(obj);
         System.out.println(seller);
 
         sc.close();
