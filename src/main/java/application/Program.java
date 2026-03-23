b4388dfc0e5a87bce926bd1a311e0804aaa96214
Pedro Henrique Hermes <pedrohhermes2006@gmail.com>
2026-03-23 10:19:09 -0300

    Seller entity class

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -9,9 +12,12 @@ public class Program {
     public static void main(String[] args){
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.parse("15/10/2000", fmt), 3000.00, obj);
+        
+        System.out.println(seller);
 
         sc.close();
     }
