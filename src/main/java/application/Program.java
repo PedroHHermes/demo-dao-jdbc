51d593e04fb9fc6dd82e4341d7fd56f8e8f3ee16
Pedro Henrique Hermes <pedrohhermes2006@gmail.com>
2026-03-23 10:02:32 -0300

    Department entity class

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+import model.entities.Department;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+
+        sc.close();
+    }
+}
